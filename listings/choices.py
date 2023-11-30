f72c24c596de9dbfaec771164e112a372098a32f
zebamahfuz <zebamahfuz@gmail.com>
2023-11-30 18:05:12 +0000

    initial commit

---
+++ listings/choices.py
@@ -0,0 +1,51 @@
+bedroom_choices = {
+  '1':1,
+  '2':2,
+  '3':3,
+  '4':4,
+  '5':5,
+  '6':6,
+  '7':7,
+  '8':8,
+  '9':9,
+  '10':10
+  }
+
+price_choices = {
+  '100000':'€100,000',
+  '200000':'€200,000',
+  '300000':'€300,000',
+  '400000':'€400,000',
+  '500000':'€500,000',
+  '600000':'€600,000',
+  '700000':'€700,000',
+  '800000':'€800,000',
+  '900000':'€900,000',
+  '1000000':'€1M+',
+}
+
+state_choices = {
+        
+       'AP': 'Carlow',
+       'HYD' : 'Cavan' ,
+       'AP' : 'Clare',
+       'AS':'Cork',
+       'BH':'Donegal',
+       'P':'Dublin',
+       'CHG':'Galway',
+       'Rp':'Kerry',
+       'G': 'Kildare',
+       'GU':'Kilkenny',
+       'H': 'Limerick',
+       'HP': 'Mayo',
+       'JAK'  : 'Monaghan',
+       'JK': 'Roscommon',
+       'RCH': 'Offaly',
+       'KR': 'Meath',
+       'KRL' : 'Louth',
+       'MP' : 'Waterford',
+       'BP' :  'Westmeath',
+       'MP':'Wexford',
+       'OD' :  'Wicklow'
+       
+}
